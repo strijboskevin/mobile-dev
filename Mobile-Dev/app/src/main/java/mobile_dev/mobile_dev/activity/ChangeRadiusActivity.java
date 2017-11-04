b2de75e3f828053dfd887cc5d89f08a37f26bb9e
Frédérique Van Wonterghem <fredvw_d12@hotmail.com>
2017-11-04 23:26:45 +0100

    Fixed change radius preferences

--- Mobile-Dev/app/src/main/java/mobile_dev/mobile_dev/activity/ChangeRadiusActivity.java
+++ Mobile-Dev/app/src/main/java/mobile_dev/mobile_dev/activity/ChangeRadiusActivity.java
@@ -19,14 +19,15 @@ public class ChangeRadiusActivity extends AppCompatActivity {
     @BindView(R.id.change_radius_seekbar_text) TextView textView;
     private User user;
     private int radius = 0;
+    private static SharedPreferences prefs;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.xml.radius_preferences);
+        super.onCreate(savedInstanceState);;
         this.user = ((UserContainer) getIntent().getSerializableExtra("user")).getUser();
+        setContentView(R.layout.activity_change_radius);
         ButterKnife.bind(this);
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        prefs = getSharedPreferences("prefs", MODE_PRIVATE);
         seekBar.setProgress(prefs.getInt("seekBarValue", 0));
         textView.setText(prefs.getString("textViewValue", "0km"));
         setSeekBar();
@@ -38,7 +39,7 @@ public class ChangeRadiusActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int radius, boolean b) {
                 textView.setText(radius + "km");
-                SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+                prefs = getSharedPreferences("prefs", MODE_PRIVATE);
                 SharedPreferences.Editor editor = prefs.edit();
                 editor.putInt("seekBarValue", radius);
                 editor.putString("textViewValue", radius + "km");
